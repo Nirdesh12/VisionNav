30b8cc0ce66e6eeab34b7ffecfeb3567c6af17a2
nirdesh01 <nirdeshpudasaini2001@gmail.com>
2025-11-30 19:22:40 +0545

    Initial Commit

---
+++ VisionNav/VisionNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionNavApp.swift
+//  VisionNav
+//
+//  Created by Nirdesh Pudasaini on 30/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct VisionNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisionNavTests/VisionNavTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionNavTests.swift
+//  VisionNavTests
+//
+//  Created by Nirdesh Pudasaini on 30/11/2025.
+//
+
+import Testing
+@testable import VisionNav
+
+struct VisionNavTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
